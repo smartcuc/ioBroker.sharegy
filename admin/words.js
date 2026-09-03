ee7f0e15ef9ceec275ec7ccfec1cc780816aa2bd
Ruediger Koenen <ruediger@coach4success.eu>
2026-09-03 23:34:48 +0200

    fix(iobroker): fix admin jsonConfig root structure to type tabs and add words.js dictionary

---
+++ admin/words.js
@@ -0,0 +1,25 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "sharegy adapter settings": {
+        "en": "Adapter settings for Sharegy Energy Management",
+        "de": "Adaptereinstellungen für Sharegy Energiemanagement"
+    },
+    "Connection": {
+        "en": "Connection",
+        "de": "Verbindung"
+    },
+    "EMS & Core Metrics": {
+        "en": "EMS & Core Metrics",
+        "de": "EMS & Kern-Zähler"
+    },
+    "Custom Devices & Sensors": {
+        "en": "Custom Devices & Sensors",
+        "de": "Sonstige Geräte & Sensoren"
+    },
+    "Bidirectional Control": {
+        "en": "Bidirectional Control",
+        "de": "Rückkanal & Lastmanagement"
+    }
+};
